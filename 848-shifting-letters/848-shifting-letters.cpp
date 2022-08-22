f4b00d2bcfe0748e3bda89a551ab745f0ed82d48
Himanshu Suryawanshi <97045316+himanshusuryawanshi@users.noreply.github.com>
2022-08-22 19:26:51 +0530

    Time: 235 ms (36.21%), Space: 67.8 MB (57.18%) - LeetHub

--- 848-shifting-letters/848-shifting-letters.cpp
+++ 848-shifting-letters/848-shifting-letters.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     string shiftingLetters(string s, vector<int>& shifts) {
-        int shiftTimes = 0;
-        for (int i = s.size() - 1; i >= 0; --i)
-        {
-            shiftTimes = (shiftTimes + shifts[i]) % 26;
-            s[i] = (s[i] - 'a' + shiftTimes) % 26 + 'a';
+        long shift=0;
+        for(int i=s.size()-1;i>=0;i--){
+            s[i]=((s[i]-'a')+(shift+shifts[i]) % 26) % 26 +'a';
+            shift+=shifts[i];
         }
         return s;
-
     }
 };
