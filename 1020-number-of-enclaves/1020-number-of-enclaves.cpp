4b9bc277ab3a0055b01a5620b334ec1278d7e759
Himanshu Suryawanshi <97045316+himanshusuryawanshi@users.noreply.github.com>
2022-09-04 20:08:55 +0530

    Time: 184 ms (5.76%), Space: 27.7 MB (8.87%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -23,22 +23,30 @@ class Solution {
         }
     }
     
+    
+    
     public:
     int numEnclaves(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
         
         vector<vector<int>> vis(n, vector<int>(m,0));
+        queue<pair<int,int>> q;
+        q.push({0,0});
         
         for(int j=0;j<m;j++)
         {
             if(!vis[0][j] && grid[0][j]==1)
             {
-                dfs(0,j,vis,grid);
+                //dfs(0,j,vis,grid);
+                vis[0][j] = 1;
+                q.push({0,j});
             }
             if(!vis[n-1][j] && grid[n-1][j]==1)
             {
-                dfs(n-1,j,vis,grid);
+                //dfs(n-1,j,vis,grid);
+                vis[n-1][j] = 1;
+                q.push({n-1,j});
             }
         }
         
@@ -46,13 +54,42 @@ class Solution {
         {
             if(!vis[i][0] && grid[i][0] ==1)
             {
-                dfs(i,0,vis,grid);
+                //dfs(i,0,vis,grid);
+                vis[i][0] = 1;
+                q.push({i,0});
             }
             if(!vis[i][m-1] && grid[i][m-1]==1)
             {
-                dfs(i,m-1,vis,grid);
+                //dfs(i,m-1,vis,grid);
+                vis[i][m-1] = 1;
+                q.push({i,m-1});
             }
         }
+        
+        while(!q.empty())
+        {
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+            
+            int delr[] = {-1, 0, +1, 0};
+            int delc[] = {0, -1, 0, +1};
+            
+            for(int i=0;i<4;i++)
+            {
+                int nrow = row + delr[i];
+                int ncol = col + delc[i];
+                
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol] && grid[nrow][ncol]==1)
+                {
+                    vis[nrow][ncol] = 1;
+                    q.push({nrow,ncol});
+                }
+            }
+            
+        }
+        
+        
         
        // int totc = 0;
         int ans = 0;
