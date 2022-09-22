9a4a5eba5d14f7e7767800f6034ef8fc2d339e60
Himanshu Suryawanshi <97045316+himanshusuryawanshi@users.noreply.github.com>
2022-09-22 18:11:20 +0530

    Time: 26 ms (45.88%), Space: 9.6 MB (90.16%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -1,6 +1,29 @@
 class Solution {
 public:
+    string reverseWords(string s)
+    {
+        //T.C O(N)
+        //S.C O(1)
+        int space = 0;
+        
+        for(int i=0;i<=s.length();i++)
+        {
+            if(i==s.length() || s[i]==' ')
+            {
+                reverse(&s[space],&s[i]);
+                space = i+1;
+            }
+        }
+        return s;
+        
+    }
+    
+    
+    /*
     string reverseWords(string s) {
+     //TC O(N)
+        //SC O(N) because of the auxiliary space cuz of using stack
+        
         stack<char>st;
         string z="";
         for(int i=0;i<s.length();i++){
@@ -20,4 +43,5 @@ public:
         }
         return z;
     }
+    */
 };
