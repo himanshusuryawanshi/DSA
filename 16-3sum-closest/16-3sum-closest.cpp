a7a6a86ab3bf42ac530f814259f447a8f8d14363
Himanshu Suryawanshi <97045316+himanshusuryawanshi@users.noreply.github.com>
2022-10-08 22:59:40 +0530

    Time: 545 ms (32.74%), Space: 16.4 MB (48.38%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.cpp
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -1,31 +1,27 @@
 
 class Solution {
 public:
-    int threeSumClosest(vector<int>& a, int target) {
-        sort(a.begin(),a.end());   // -4 -1 1 2
-        int ans = INT_MAX;
-        int mn = INT_MAX;
-        for(int i = 0;i<a.size();i++)
-        {
-            int x = a[i];
-            int low = i+1;
-            int high = a.size()-1;
-            while(low<high)
-            {
-                int y = a[low] + a[high];
-                if(x+y-target>0)
-                {
-                    high--;
-                }
-                else 
-                    low++;
-                if(abs(x+y-target)<mn)
-                {
-                    ans = x + y;
-                    mn = abs(x+y-target);
-                }
+   int threeSumClosest(vector<int>& nums, int target) {
+    if(nums.size() < 3) return 0;
+    int closest = nums[0]+nums[1]+nums[2];
+    sort(nums.begin(), nums.end());
+    for(int first = 0 ; first < nums.size()-2 ; ++first) {
+        if(first > 0 && nums[first] == nums[first-1]) continue;
+        int second = first+1;
+        int third = nums.size()-1;            
+        while(second < third) {
+            int curSum = nums[first]+nums[second]+nums[third];
+            if(curSum == target) return curSum;
+            if(abs(target-curSum)<abs(target-closest)) {
+                closest = curSum;
+            }
+            if(curSum > target) {
+                --third;
+            } else {
+                ++second;
             }
         }
-        return ans;
     }
+    return closest;
+}
 };
