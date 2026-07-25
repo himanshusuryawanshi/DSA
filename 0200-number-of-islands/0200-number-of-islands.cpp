7215c873cb78bc368695826d3d5f38c5516989e7
Himanshu Suryawanshi <97045316+himanshusuryawanshi@users.noreply.github.com>
2026-07-25 20:51:28 +0530

    Time: 35 ms (14.6%), Space: 23.7 MB (23.9%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,26 +1,5 @@
 class Solution {
 public:
-//DFS
-    void dfs(vector<vector<char>>& grid, int row, int col,vector<vector<bool>>& visited){
-        visited[row][col] = true;
-        int n=grid.size();int m=grid[0].size();
-        if((col-1>=0) && !visited[row][col-1] && grid[row][col-1]=='1'){
-           visited[row][col-1] = true;
-           dfs(grid,row,col-1,visited);
-        }
-        if((col+1<m) && !visited[row][col+1] && grid[row][col+1]=='1'){
-           visited[row][col+1] = true;
-           dfs(grid,row,col+1,visited);
-        }
-        if((row-1>=0) && !visited[row-1][col] && grid[row-1][col]=='1'){
-           visited[row-1][col] = true;
-           dfs(grid,row-1,col,visited);
-        }
-        if((row+1<n) && !visited[row+1][col] && grid[row+1][col]=='1'){
-           visited[row+1][col] = true;
-           dfs(grid,row+1,col,visited);
-        }
-    }
     int numIslands(vector<vector<char>>& grid) {
         int n=grid.size();int m=grid[0].size();
         vector<vector<bool>> visited(n,vector<bool>(m,false));
@@ -29,7 +8,34 @@ public:
             for(int j=0;j<grid[0].size();j++){
                 if(grid[i][j]=='1' and !visited[i][j]){
                     ans++;
-                    dfs(grid,i,j,visited);
+                    queue<pair<int,int>> q;
+                    q.push({i,j});
+                    visited[i][j] = true;
+                    while(!q.empty()){
+                        int row = q.front().first;
+                        int col = q.front().second;
+                        q.pop();
+                        //left
+                        if((col-1>=0) && !visited[row][col-1] && grid[row][col-1]=='1'){
+                            visited[row][col-1] = true;
+                            q.push({row,col-1});
+                        }
+                        //right
+                        if((col+1<m) && !visited[row][col+1] && grid[row][col+1]=='1'){
+                            visited[row][col+1] = true;
+                            q.push({row,col+1});
+                        }
+                        //up
+                        if((row-1>=0) && !visited[row-1][col] && grid[row-1][col]=='1'){
+                            visited[row-1][col] = true;
+                            q.push({row-1,col});
+                        }
+                        //down
+                        if((row+1<n) && !visited[row+1][col] && grid[row+1][col]=='1'){
+                            visited[row+1][col] = true;
+                            q.push({row+1,col});
+                        }
+                    }
                 }
             }
         }
